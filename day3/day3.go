25d3ced0e9b72dd7ab3b02b25dd1f26ea86ad6a6
Mike Conroy <mikecon@outlook.com>
2025-12-04 21:12:34 +0000

    Day 3 - P2

--- day3/day3.go
+++ day3/day3.go
@@ -2,6 +2,7 @@ package day3
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/mikeconroy/advent-of-code-25/utils"
 )
@@ -33,14 +34,51 @@ func getMaxJoltage(bank string) int {
 	return maxJolt
 }
 
+func getMaxJolts(bank string, joltsRequired int, level int, cache map[string]int) int {
+	key := bank + "-" + strconv.Itoa(joltsRequired)
+	if cache[key] != 0 {
+		return cache[key]
+	}
+	// fmt.Println(strings.Repeat(" ", level), "Bank:", bank, "JoltsRequired:", joltsRequired)
+	// Base Case - if no more need to be activated we return the current bank
+	// If the amount to be activated is the length of the remaining batteries we return that value.
+	if len(bank) <= joltsRequired {
+		result, _ := strconv.Atoi(bank)
+		return result
+	} else {
+		// Compare Max Jolts if we take the first value vs. continuing
+		var firstDigitResult int
+		if joltsRequired-1 == 0 {
+			firstDigitResult = int(bank[0]) - '0'
+		} else {
+			firstDigitResult, _ = strconv.Atoi(string(bank[0]) + strconv.Itoa(getMaxJolts(bank[1:len(bank)], joltsRequired-1, level+1, cache)))
+		}
+		withoutFirstDigit := getMaxJolts(bank[1:len(bank)], joltsRequired, level+1, cache)
+		// fmt.Println(strings.Repeat(" ", level), "Bank:", bank, "JoltsReqs:", joltsRequired, "First:", firstDigitResult, "Without:", withoutFirstDigit, bank[1:len(bank)])
+		if firstDigitResult > withoutFirstDigit {
+			cache[key] = firstDigitResult
+			return firstDigitResult
+		} else {
+			cache[key] = withoutFirstDigit
+			return withoutFirstDigit
+		}
+	}
+}
+
 func part1(input []string) string {
 	totalJoltage := 0
 	for _, bank := range input {
-		totalJoltage += getMaxJoltage(bank)
+		result := getMaxJolts(bank, 2, 0, make(map[string]int))
+		totalJoltage += result
 	}
 	return fmt.Sprint(totalJoltage)
 }
 
 func part2(input []string) string {
-	return fmt.Sprint(0)
+	memoisedJolts := make(map[string]int)
+	totalJoltage := 0
+	for _, bank := range input {
+		totalJoltage += getMaxJolts(bank, 12, 0, memoisedJolts)
+	}
+	return fmt.Sprint(totalJoltage)
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -17,7 +17,7 @@ func TestDay3Part1(t *testing.T) {
 
 func TestDay3Part2(t *testing.T) {
 	input := utils.ReadFileIntoSlice("input_test")
-	expected := "0"
+	expected := "3121910778619"
 	result := part2(input)
 	if result != expected {
 		t.Fatal("Day 3 - Part 2 output should be", expected, "but got", result)
